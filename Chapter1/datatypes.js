d8834bea5db11b8ea00a66be4883ffea98956573
Aishwarya Anand <aishwarya.anand1125@gmail.com>
2025-04-16 15:25:49 +0000

    datatypes

---
+++ Chapter1/datatypes.js
@@ -0,0 +1,36 @@
+"use strict"  //used to treat all js code as newer version
+
+//alert(3+3)   // alert is not defined here coz it's node.js not browser..
+
+let myName='aishwarya'
+let age=20
+let isLoggedin=true
+let state;
+let city=null
+
+console.log(typeof myName) //string
+console.log(typeof age)   //number
+console.log(typeof isLoggedin)  //boolean
+console.log(typeof state)  //undefined
+console.log(typeof city)  //null-->object
+
+
+
+/*
+SUMMARY:
+
+primitive datatypes
+1.string
+2.number
+3.boolean
+4.undefined
+5.null
+6.bigInt
+7.symbol
+
+Non-primitive:
+1.Object
+2.Arrays
+3.Functions
+
+*/
